89dc5f3a532fb552250a3c02b8dc897c33189424
Gavin Szczesniak <szczesg@ferris.edu>
2021-04-11 19:50:35 -0400

    Boss can aim and has the ability to shoot
    * Added more to the BossStateMachine, the boss can aim its cannon at the player and shoots a raycast out that hits the player to know when to shoot. 
    
    I plan to make the boss shoot projectiles and stop moving at a certain distance from the player next push

--- Assets/Rigs/TurretSpider/Script/BossStateMachine.cs
+++ Assets/Rigs/TurretSpider/Script/BossStateMachine.cs
@@ -33,8 +33,7 @@ public class BossStateMachine : MonoBehaviour {
                 bossState.IdleAnim();
 
                 // transition: 
-                if (bossState.targetPlayer) {
-                    bossState.bossNav.isStopped = false;
+                if (bossState.TargetDistance(bossState.targetMeterDistance)) {
                     return new States.Attack();
                 }
 
@@ -53,13 +52,24 @@ public class BossStateMachine : MonoBehaviour {
         public class Attack : State {
 
             public override State Update() {
-                // behaviour
+                // behaviour:
                 bossState.bossNav.SetDestination(bossState.targetPlayer.position);
+                bossState.LookAtTarget();
 
-                if (!bossState.targetPlayer) {
-                    bossState.bossNav.isStopped = true;
+
+                Debug.DrawRay(bossState.barrel.position, bossState.barrel.TransformDirection(Vector3.forward) * 35);
+                if (bossState.TargetDistance(bossState.targetMeterDistance - 5) && Physics.Raycast(bossState.barrel.position, bossState.barrel.TransformDirection(Vector3.forward), out RaycastHit hit, bossState.AttackDistance)) {
+
+                    if (hit.collider.gameObject.CompareTag("Player"))
+                        bossState.AttackAction();
+                }
+
+                // transition:
+                if (!bossState.TargetDistance(bossState.targetMeterDistance)) {
                     return new States.Idle();
                 }
+
+
 
                 return null;
             }
@@ -84,6 +94,12 @@ public class BossStateMachine : MonoBehaviour {
 
     public Transform targetPlayer;
 
+    public Transform barrel;
+
+    public float targetMeterDistance = 10;
+
+    public float AttackDistance = 35;
+
     void Start() {
         bossNav = GetComponent<NavMeshAgent>();
     }
@@ -106,5 +122,32 @@ public class BossStateMachine : MonoBehaviour {
     void IdleAnim() {
         hoverBody.localPosition = Vector3.down * .25f * Mathf.Cos(Time.time);
         hoverBody.localRotation = Quaternion.Euler(2f * Mathf.Sin(Time.time), 4f * Mathf.Sin(Time.time), 2f * Mathf.Cos(Time.time));
+    }
+
+    private bool TargetDistance(float targetDis) {
+
+        if (!targetPlayer) return false;
+
+        Vector3 disToTarget = targetPlayer.position - transform.position;
+
+        if (disToTarget.sqrMagnitude > targetDis * targetDis) {
+            bossNav.isStopped = true;
+            return false;
+        }
+
+        bossNav.isStopped = false;
+        return true;
+    }
+
+    void LookAtTarget() {
+        if (!targetPlayer) return;
+
+        print("trying to look");
+        Quaternion rotateTowards = Quaternion.LookRotation((targetPlayer.position - hoverBody.position) - new Vector3(0, 6, 0));
+        hoverBody.rotation = Quaternion.Slerp(hoverBody.rotation, rotateTowards, 1 * Time.deltaTime); //hoverBody.LookAt(targetPlayer.position - new Vector3(0, 5, 0));
+    }
+
+    void AttackAction() {
+        print("bang");
     }
 }
